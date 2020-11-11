2574092285fddbf79edb4cb3c4ac0f6a49f5fec9
Adem-hub <57374875+Adem-hub@users.noreply.github.com>
2020-11-11 21:03:43 +0100

    Add files via upload

--- Projet Pronote.py
+++ Projet Pronote.py
@@ -3,22 +3,41 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
+import os
+import requests
+import json
+
 
 options = Options()
 options.add_argument('--headless')
 
 
-
+os.chdir("C:\\Users\\Ridha\\Desktop\\Pronote")
 
 
 
 
 class Pronote:
-    def __init__(self,ide,mdp):
+    def __init__(self,ide,mdp,token_pushbullet):
         self.identifiant=ide
         self.mdp=mdp
+        self.token_pushbullet=token_pushbullet
         self.driver = webdriver.Firefox(options=options,executable_path='C://Users//Ridha//.wdm//drivers//geckodriver//win64//v0.28.0//geckodriver.exe')
+        self.Aller_Sur_Pronote()
+        self.Onglet_Moyenne()
+        self.Analyse_Nouvelle_Note()
 
+    def pushbullet_message(self,title, body):
+        msg = {"type": "note", "title": title, "body": body}
+        TOKEN = 'PUSHBULLET_TOKEN'
+        resp = requests.post('https://api.pushbullet.com/v2/pushes',
+                            data=json.dumps(msg),
+                            headers={'Authorization': 'Bearer ' + self.token_pushbullet,
+                                    'Content-Type': 'application/json'})
+        if resp.status_code != 200:
+            raise Exception('Error',resp.status_code)
+        else:
+            print ('Message sent')
 
     def Aller_Sur_Pronote(self):
         self.driver.get("https://ent.iledefrance.fr/auth/login?callback=https%3A%2F%2Fent.iledefrance.fr%2Ftimeline%2Ftimeline")
@@ -33,6 +52,7 @@ class Pronote:
         time.sleep(5)
 
         apps=self.driver.find_elements_by_class_name('apps')[0]
+
         apps.click()
         time.sleep(5)
         pronote=self.driver.find_elements_by_class_name('pronote')[0]
@@ -53,7 +73,7 @@ class Pronote:
             print("tu dois d'abord aller sur Pronote!")
 
     def Moyenne(self,nom):
-
+        time.sleep(5)
         a=self.driver.page_source
         soup = BeautifulSoup(a)
         b=soup.find_all("div", class_="Gras Espace")
@@ -66,8 +86,46 @@ class Pronote:
                 Matiere=Matiere.replace(' ','')
                 Dico[Matiere]=Note
         if Dico=={}:
-            return "est tu allé sur l'onglet Moyenne ?"
+            return "est tu allé sur l'onglet Moyenne ? Si, oui, alors la moyenne n'existe pas"
         return Dico
+
+    def Analyse_Nouvelle_Note(self):
+        time.sleep(5)
+        tmp1= self.driver.find_elements_by_class_name("objetbandeauentete_global")
+        time.sleep(5)
+        chronologique=tmp1[0].find_elements_by_id("id_1481")
+        chronologique[0].click()
+
+        page=self.driver.page_source
+        soup = BeautifulSoup(page)
+        Tr_Notes=soup.find_all("tr")
+        Toutes_les_Notes=len(Tr_Notes)-1
+
+        f = open("NombreNotes.txt", "r+")
+        New=f.read()
+        Nbre_Notes=int(New[-2:])
+
+
+        #On send la notif
+        if Toutes_les_Notes!=Nbre_Notes:
+            self.pushbullet_message('Pronote','Une nouvelle note est apparue !')
+            f.truncate(0)
+            f.write(str(Toutes_les_Notes))
+            f.close()
+        else:
+            print('aucune note')
+
+def Look_New_Mark():
+    Adem=Pronote('adem.benrhouma','Adem2003@',"o.J669zeEpYeZpHcWte0C0tt5ONmkXgZsr")
+    Adem.driver.quit()
+
+
+
+
+
+
+
+
 
 
 
