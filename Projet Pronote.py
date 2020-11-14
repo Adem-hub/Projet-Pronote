703881330f37cb7f1bf85d4e90b55a5f7bf4e7e0
Adem-hub <57374875+Adem-hub@users.noreply.github.com>
2020-11-14 14:53:18 +0100

    Add files via upload

--- Projet Pronote.py
+++ Projet Pronote.py
@@ -13,7 +13,7 @@ options.add_argument('--headless')
 
 
 os.chdir("C:\\Users\\Ridha\\Desktop\\Pronote")
-
+path="C:\\Users\\Ridha\\Desktop\\Pronote"
 
 
 
@@ -25,7 +25,7 @@ class Pronote:
         self.driver = webdriver.Firefox(options=options,executable_path='C://Users//Ridha//.wdm//drivers//geckodriver//win64//v0.28.0//geckodriver.exe')
         self.Aller_Sur_Pronote()
         self.Onglet_Moyenne()
-        self.Analyse_Nouvelle_Note()
+
 
     def pushbullet_message(self,title, body):
         msg = {"type": "note", "title": title, "body": body}
@@ -37,7 +37,7 @@ class Pronote:
         if resp.status_code != 200:
             raise Exception('Error',resp.status_code)
         else:
-            print ('Message sent')
+            print ('Message envoyé')
 
     def Aller_Sur_Pronote(self):
         self.driver.get("https://ent.iledefrance.fr/auth/login?callback=https%3A%2F%2Fent.iledefrance.fr%2Ftimeline%2Ftimeline")
@@ -49,7 +49,7 @@ class Pronote:
         id.send_keys(self.identifiant)
         password.send_keys(self.mdp)
         password.submit()
-        time.sleep(5)
+        time.sleep(8)
 
         apps=self.driver.find_elements_by_class_name('apps')[0]
 
@@ -64,57 +64,58 @@ class Pronote:
 
     def Onglet_Moyenne(self):
         try:
-            time.sleep(7)
-            Notes=self.driver.find_elements_by_id("GInterface.Instances[0].Instances[1]_Combo2")[0]
-            Notes.click()
             time.sleep(5)
+            Notes=self.driver.find_elements_by_class_name("label-menu_niveau0")[3]
+            Notes.click()
+            time.sleep(3)
             print("Tres bien, maintenant va chercher tes moyennes avec la methode Moyenne")
+            self.Analyse_Nouvelle_Note()
+            self.driver.quit()
         except:
-            print("tu dois d'abord aller sur Pronote!")
+            print("Une petite erreur visiblement!")
+            self.driver.quit()
 
-    def Moyenne(self,nom):
-        time.sleep(5)
-        a=self.driver.page_source
-        soup = BeautifulSoup(a)
-        b=soup.find_all("div", class_="Gras Espace")
-        Dico={}
-        for elt in b:
-            if nom.upper() in elt['aria-label']:
-                hey=elt.find('div',style="float: right;")
-                Note=hey.text
-                Matiere=elt.text.replace(Note,'')
-                Matiere=Matiere.replace(' ','')
-                Dico[Matiere]=Note
-        if Dico=={}:
-            return "est tu allé sur l'onglet Moyenne ? Si, oui, alors la moyenne n'existe pas"
-        return Dico
 
     def Analyse_Nouvelle_Note(self):
-        time.sleep(5)
-        tmp1= self.driver.find_elements_by_class_name("objetbandeauentete_global")
-        time.sleep(5)
-        chronologique=tmp1[0].find_elements_by_id("id_1481")
-        chronologique[0].click()
-
+        #On se place dans le button radio chronologique, et on compte le nombre de notes
+        Label=self.driver.find_elements_by_tag_name("label")
+        Label[2].click()
+        time.sleep(3)
         page=self.driver.page_source
         soup = BeautifulSoup(page)
         Tr_Notes=soup.find_all("tr")
         Toutes_les_Notes=len(Tr_Notes)-1
 
-        f = open("NombreNotes.txt", "r+")
-        New=f.read()
-        Note=New.replace(' ','')
-        Nbre_Notes=int(Note)
+        #On ecrit le nombre de notes dans le fichier qui correspond. Si il n'existe pas, on le crée.
+        try:
+            with open(self.identifiant+'.txt','r') as file:
+                len_Notes=int(file.read())
+                file.close()
+        except:
+            with open(self.identifiant+'.txt','w+') as file:
+                file.write(str(Toutes_les_Notes))
+                len_Notes=Toutes_les_Notes
+                file.close()
 
-
-        #On send la notif
-        if Toutes_les_Notes!=Nbre_Notes:
-            self.pushbullet_message('Pronote','Une nouvelle note est apparue !')
-            f.truncate(0)
-            f.write(str(Toutes_les_Notes))
-            f.close()
+        #On send la ou les notif(s)
+        if Toutes_les_Notes!=int(len_Notes):
+            for i in range(Toutes_les_Notes -int(len_Notes)):
+                self.pushbullet_message('Pronote : Une nouvelle note est apparue !', str(Tr_Notes[i+1].get_text()))
+            with open(self.identifiant+'.txt','w') as file:
+                file.write(str(Toutes_les_Notes))
+                file.close()
         else:
-            print('aucune note')
+            print('Rien de neuf')
+
+
+
+
+
+def Look_New_Mark():
+    while True:
+        Othman=Pronote('othman.boudarga','Boudarga31082005',"o.J669zeEpYeZpHcWte0C0tt5ONmkXgZsr")
+        Adem=Pronote('adem.benrhouma','Adem2003@',"o.J669zeEpYeZpHcWte0C0tt5ONmkXgZsr")
+        time.sleep(1200)
 
 
 
